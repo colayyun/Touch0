552302e328cdeda3e97290e19629988fc67f42b5
colayyun <koyun0902@gmail.com>
2025-01-02 23:27:56 +0800

    洪可芸的第三次commit

--- app/src/main/java/tw/edu/pu/s1110054/touch0/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1110054/touch0/MainActivity.kt
@@ -3,20 +3,26 @@ package tw.edu.pu.s1110054.touch0
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 import tw.edu.pu.s1110054.touch0.ui.theme.Touch0Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Touch0Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,16 +38,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    androidx.compose.material3.Text(
+        text = "Hello, $name! Welcome to Jetpack Compose!",
+        fontFamily = FontFamily(Font(R.font.finger)), // 確保 finger.ttf 存在於 res/font 資料夾
+        fontSize = 25.sp,
+        color = Color.Blue,
         modifier = modifier
+    )
+
+    Image(
+        painter = painterResource(id = R.drawable.hand),
+        contentDescription = "手掌圖片",
+        alpha = 0.7f,
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(Color.Blue)
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Touch0Theme {
-        Greeting("Android")
-    }
-}
